320f01b4e1784e45a9a1ea51a7ccdd0e85a73f64
mayad19-meet <mayad19@meet.mit.edu>
2017-07-26 12:23:28 +0300

    Stopping here

---
+++ fruit_sorter.py
@@ -0,0 +1,10 @@
+new_fruit=input('what fruit am i storing?')
+if new_fruit=='apples':
+    print('bin 1!')
+elif new_fruit=='oranges':
+    print('bin 2!')
+elif new_fruit=='olives':
+    print('bin 3!')
+else:
+    print('error!! i do not recognize this fruit!')
+    
---
+++ tutle_party.py
@@ -0,0 +1,17 @@
+straberries=50
+is_weekend=False
+print(input('is this a weekend or a week_day?'))
+if 'weekend':
+    print(input('how mach straberries you have?'))
+if straberries>39:
+    print('fun!')
+else:
+    print('not fun!')
+if 'week_day':
+    print(input('how mach straberries you have?'))
+if straberries>39 and less61:
+    print('fun')
+else:
+    print('not fun')
+    
+     
